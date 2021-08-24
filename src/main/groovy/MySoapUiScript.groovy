226f8fded5a431ae3bd430a0c84aab3481b2fcd1
Patrick Holzer <patrick.holzer@gepardec.com>
2021-08-24 16:46:49 +0200

    Inital commit

---
+++ src/main/groovy/MySoapUiScript.groovy
@@ -0,0 +1,7 @@
+import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCaseRunner
+import org.apache.log4j.Logger
+
+testRunner = (WsdlTestCaseRunner) testRunner;
+log = (Logger) log;
+
+// add your code here
