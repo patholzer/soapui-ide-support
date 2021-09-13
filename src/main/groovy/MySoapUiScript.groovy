8c5e4508d1900e3b828de11be6a21a160adaec7c
Patrick Holzer <patrick.holzer@gepardec.com>
2021-09-13 11:28:09 +0200

    Add context variable

--- src/main/groovy/MySoapUiScript.groovy
+++ src/main/groovy/MySoapUiScript.groovy
@@ -1,7 +1,9 @@
+import com.eviware.soapui.impl.wsdl.panels.support.MockTestRunContext
 import com.eviware.soapui.impl.wsdl.testcase.WsdlTestCaseRunner
 import org.apache.log4j.Logger
 
 testRunner = (WsdlTestCaseRunner) testRunner;
 log = (Logger) log;
+context = (MockTestRunContext) context;
 
 // add your code here
